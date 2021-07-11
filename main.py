3a6e089b4d7a80103bec61f1ea66612f3c6ac9d9
Ashish Maurya <ashishmaurya@outlook.com>
2021-07-12 00:29:34 +0530

    Added comments

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 import tensorflow as tf
 from tensorflow import keras
-# from tensorflow.python.keras.backend import conv2d
 
+# Load images in dataset (so that we don't have to load all imagees in memory at once)
 dataset = keras.preprocessing.image_dataset_from_directory(
     './data/train_py', image_size=(1920, 1080),batch_size=1)
-# size = (200, 200)
-# dataset = dataset.map(lambda img: tf.image.resize(img, size))
+
+# create model
 model = keras.Sequential()
+# add preprocessing layer to reduce the image size(to reduce computation complexity)
 model.add(tf.keras.layers.experimental.preprocessing.Resizing(640,360))
+# add Convulation Layer(good for image classification)
 model.add(tf.keras.layers.Conv2D(
     128, 3, activation='sigmoid', input_shape=(640, 360, 3),batch_size=1))
-# model.add(tf.keras.layers.MaxPool2D())
-# model.add(tf.keras.layers.Dropout(0.4))
-# model.add(tf.keras.layers.Conv2D(
-#     64, 3, activation='sigmoid', input_shape=(640, 360, 3),batch_size=1))
-# model.add(tf.keras.layers.MaxPool2D())
-# model.add(tf.keras.layers.Dropout(0.4))
-# model.add(tf.keras.layers.Conv2D(
-#     32, 3, activation='sigmoid', input_shape=(640, 360, 3),batch_size=1))
-# model.add(tf.keras.layers.MaxPool2D())
-# model.add(tf.keras.layers.Dropout(0.4))
+# Flatten the output from above layer as softmax accepts vectors(1 dimension)
 model.add(tf.keras.layers.Flatten())
+# Add softmax layer to convert it into probability
 model.add(tf.keras.layers.Dense(2,activation='softmax'))
+# Compile  the model
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy']
 )
-# model.summary()
+
+# Train your model
 model.fit(dataset,epochs=5)
 
+# Save model onto disk
 model.save('./ts-models/py-car-cmd')
-
-# model.evaluate(dataset)
--- predict.py
+++ predict.py
@@ -1,18 +1,14 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import matplotlib.pyplot as plt
 
-dataset = keras.preprocessing.image_dataset_from_directory(
-    './data/train_py', image_size=(1920, 1080),batch_size=1)
-
+# load saved model
 model = keras.models.load_model('./ts-models/py-car-cmd')
 
-
+#Load your own  image to pridict
 image = tf.keras.preprocessing.image.load_img('data/test/car_1.jpg', target_size=(1920,1080))
 
 input_arr = tf.keras.preprocessing.image.img_to_array(image)
 input_arr = np.array([input_arr])  # Convert single image to a batch.
 predictions = model.predict(input_arr)
-# predictions = model.evaluate(dataset)
 print(predictions)
